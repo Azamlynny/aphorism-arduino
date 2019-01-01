472ebdfc2e0b1da55c55e17a6a8a578175e29ecb
Adam Zamlynny <azamlynny@hotmail.com>
2019-01-01 12:37:07 -0500

    added many features

--- Aphorism/Aphorism.ino
+++ Aphorism/Aphorism.ino
@@ -22,28 +22,59 @@ void loop() {
   buttonBState = digitalRead(buttonBPin);
   
   if (buttonAState == LOW) {
-    
     breakChromebook();
     triggered = true;
   }
 
   if (buttonBState == LOW){
-//    triggered = false;
+    triggered = false;
   }
   
   if(triggered){
     spinScreen();
-    alternateBrightness();
+    invertColors(); 
   }
 }
 
 void breakChromebook() {
-  for (int i = 0; i < 10; i++) {
+  openPages(1); 
+  delay(1000);
+  searchBar("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  delay(1000);
+  openPages(1);
+  delay(1000);
+//  chromeVox();
+  openTerminal();
+  delay(3000);
+  dockedMagnifier();
+}
+
+void openPages(int num){
+   for (int i = 0; i < num; i++) {
     Keyboard.press(KEY_LEFT_CTRL);
     Keyboard.press('t');
     delay(10);
     Keyboard.releaseAll();
   }
+}
+
+void openTerminal(){
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('t');
+  Keyboard.releaseAll();
+  invertColors();
+  spinScreen();
+  delay(3000);
+  Keyboard.print("help");
+  Keyboard.write(95);
+  Keyboard.println("advanced");
+  delay(3000);
+  Keyboard.println("top");
+}
+
+void searchBar(String input){
+  Keyboard.println(input);
 }
 
 void spinScreen(){
@@ -55,15 +86,35 @@ void spinScreen(){
   delay(500);
 }
 
-void alternateBrightness(){ //16 keypresses to do
-  if(brightness){
-    Keyboard.release(KEY_F7);
-    Keyboard.press(KEY_F6);
-    brightness = false;
-  }
-  else if (brightness == false){
-    Keyboard.release(KEY_F6);
-    Keyboard.press(KEY_F7);
-    brightness == true;    
-  }
+void invertColors(){
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_GUI); 
+  Keyboard.press('h'); 
+  Keyboard.releaseAll();
 }
+
+void chromeVox(){
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('z');
+  Keyboard.releaseAll();
+}
+void dockedMagnifier(){
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_GUI); 
+  Keyboard.press('d');
+}
+
+//No work
+//void alternateBrightness(){ //16 keypresses to do
+//  if(brightness){
+//    Keyboard.release(KEY_F7);
+//    Keyboard.press(KEY_F6);
+//    brightness = false;
+//  }
+//  else if (brightness == false){
+//    Keyboard.release(KEY_F6);
+//    Keyboard.press(KEY_F7);
+//    brightness == true;    
+//  }
+//}
