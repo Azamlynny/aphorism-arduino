054b7939df2d12e24cf2e265c26a2afe5ab8d9f5
Adam Zamlynny <azamlynny@hotmail.com>
2019-01-01 11:24:54 -0500

    Can create new windows when button pressed and then screen spins spontaneously

--- Aphorism/Aphorism.ino
+++ Aphorism/Aphorism.ino
@@ -1,11 +1,51 @@
 #include <Mouse.h>
 #include <Keyboard.h>
 
+int buttonAPin = 9;
+int buttonAState = 0;
+char ctrlKey = KEY_LEFT_GUI;
+boolean triggered = false;
+
 void setup() {
+  pinMode(buttonAPin, INPUT_PULLUP);
+
   Mouse.begin();
   Keyboard.begin();
 }
 
 void loop() {
-  
+  buttonAState = digitalRead(buttonAPin);
+
+  if (buttonAState == LOW) {
+    
+    breakChromebook();
+    triggered = true;
+  }
+
+  if(triggered){
+    spinScreen();
+  }
+}
+
+void breakChromebook() {
+  for (int i = 0; i < 10; i++) {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('t');
+    delay(10);
+    Keyboard.releaseAll();
+  }
+}
+
+void spinScreen(){
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_F3);
+  Keyboard.releaseAll();
+  delay(500);
+}
+
+void alternateBrightness(){
+
+}
 }
